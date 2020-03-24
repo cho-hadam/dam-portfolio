db98d4153c581e0b535167c9972a72f917dca6be
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-03-25 01:19:43 +0900

    cancel EventListener 추가

--- nav.js
+++ nav.js
@@ -4,6 +4,7 @@ const navLists = document.querySelector('.nav-lists');
 function init() {
     burger.addEventListener("click", () => {
         navLists.classList.toggle('nav-active');
+        burger.classList.toggle('cancel');
     })
 }
 
