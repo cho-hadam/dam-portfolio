f4cc7306901f095797ab902833540ffdbf58bca9
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-03-25 01:10:52 +0900

    모바일 burger nav 추가

---
+++ nav.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.burger');
+const navLists = document.querySelector('.nav-lists');
+
+function init() {
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle('nav-active');
+    })
+}
+
+init();
