ada891898b711118f26f16c74852f871f9268d04
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-03-27 00:40:04 +0900

    translateX 단위 조정

--- project/slide.js
+++ project/slide.js
@@ -1,24 +1,34 @@
 const img = document.querySelector('.images');
 const left = document.querySelector('.icon-left-open');
 const right = document.querySelector('.icon-right-open');
+const imgNum = document.querySelectorAll('.images img');
 
 let count = 0;
+let max = 0;
+
+function maxCounting() {
+    imgNum.forEach(() => {
+        max++;
+    })
+}
 
 function moveLeft() {
-    if(count < 2) {
+    if(count < max-1) {
         count++;
-        img.style.cssText = `transform: translateX(-${count*35}vh);`;
+        img.style.cssText = `transform: translateX(-${count*100}%);`;
     }
 }
 
 function moveRight() {
     if(count > 0) {
         count--;
-        img.style.cssText = `transform: translateX(-${count*35}vh);`;
+        img.style.cssText = `transform: translateX(-${count*100}%);`;
     }
 }
 
 function init() {
+    maxCounting();
+
     left.addEventListener('click', moveRight);
     right.addEventListener('click', moveLeft);
 }
