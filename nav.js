8415cda278fbd373bb3e2abbb720752ce893dda6
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-03-26 16:42:17 +0900

    night and day mode 추가

--- nav.js
+++ nav.js
@@ -1,10 +1,21 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-lists');
+const switchBtn = document.querySelector('.nav-lists i');
 
 function init() {
     burger.addEventListener("click", () => {
         nav.classList.toggle('nav-active');
         burger.classList.toggle('cancel');
+    })
+
+    switchBtn.addEventListener("click", () => {
+        if(switchBtn.classList.value == 'icon-moon-inv') {
+            switchBtn.classList.remove('icon-moon-inv');
+            switchBtn.classList.add('icon-moon');
+        } else {
+            switchBtn.classList.remove('icon-moon');
+            switchBtn.classList.add('icon-moon-inv');
+        }
     })
 }
 
