c180178fce3f6a9d3efb831d0e0bb1016c51c18c
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-03-26 20:24:09 +0900

    mode.js 분리

--- nav.js
+++ nav.js
@@ -1,45 +1,5 @@
 const burger = document.querySelector('.burger');
 const navLists = document.querySelector('.nav-lists');
-const switchBtn = document.querySelector('.nav-lists i');
-const body = document.querySelector('body');
-
-function loadMode() {
-    const mode = localStorage.getItem('mode');
-
-    if(mode == null) {
-        localStorage.setItem('mode', 'night');
-    } else {
-        if(mode == 'day') {
-            day();
-        } else {
-            night()
-        }
-    }
-}
-
-function changeMode() {
-    const mode = localStorage.getItem('mode');
-
-    if(mode == 'night') {
-        localStorage.setItem('mode', 'day');
-        day();
-    } else {
-        localStorage.setItem('mode', 'night');
-        night();
-    }
-}
-
-function day() {
-    body.classList.add('day');
-    switchBtn.classList.remove('icon-moon-inv');
-    switchBtn.classList.add('icon-moon');
-}
-
-function night() {
-    body.classList.remove('day');
-    switchBtn.classList.remove('icon-moon');
-    switchBtn.classList.add('icon-moon-inv');
-}
 
 function init() {
     loadMode();
