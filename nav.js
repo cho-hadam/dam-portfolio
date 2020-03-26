4766c6d35294e2a06bfbfcdf4edd726faf1bc088
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-03-26 17:48:38 +0900

    day mode 안정화

--- nav.js
+++ nav.js
@@ -3,23 +3,43 @@ const navLists = document.querySelector('.nav-lists');
 const switchBtn = document.querySelector('.nav-lists i');
 const body = document.querySelector('body');
 
+if(localStorage.getItem('mode') == null) {
+    localStorage.setItem('mode', 'night');
+} else {
+    if(localStorage.getItem('mode') == 'day') {
+        body.classList.add('day');
+        switchBtn.classList.remove('icon-moon-inv');
+        switchBtn.classList.add('icon-moon');
+    } else {
+        body.classList.remove('day');
+        switchBtn.classList.remove('icon-moon');
+        switchBtn.classList.add('icon-moon-inv');
+    }
+}
+
+function changeMode() {
+    const mode = localStorage.getItem('mode');
+
+    if(mode == 'night') {
+        body.classList.add('day');
+        localStorage.setItem('mode', 'day');
+        switchBtn.classList.remove('icon-moon-inv');
+        switchBtn.classList.add('icon-moon');
+    } else {
+        body.classList.remove('day');
+        localStorage.setItem('mode', 'night');
+        switchBtn.classList.remove('icon-moon');
+        switchBtn.classList.add('icon-moon-inv');
+    }
+}
+
 function init() {
     burger.addEventListener("click", () => {
         navLists.classList.toggle('nav-active');
         burger.classList.toggle('cancel');
     })
 
-    switchBtn.addEventListener("click", () => {
-        body.classList.toggle('day');
-        
-        if(switchBtn.classList.value == 'icon-moon-inv') {
-            switchBtn.classList.remove('icon-moon-inv');
-            switchBtn.classList.add('icon-moon');
-        } else {
-            switchBtn.classList.remove('icon-moon');
-            switchBtn.classList.add('icon-moon-inv');
-        }
-    })
+    switchBtn.addEventListener("click", changeMode);
 }
 
 init();
