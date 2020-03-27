c4314e6393cb438765a664c2ee46426624563be0
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-03-27 20:39:10 +0900

    dropdown menu 추가

--- nav.js
+++ nav.js
@@ -1,11 +1,24 @@
 const burger = document.querySelector('.burger');
 const navLists = document.querySelector('.nav-lists');
+const navPjt = document.querySelector('.nav-project a');
+const dropdown = document.querySelector('.dropdown');
+const pjtList = document.querySelectorAll('.dropdown a');
 
 function init() {
     burger.addEventListener("click", () => {
         navLists.classList.toggle('nav-active');
         burger.classList.toggle('cancel');
     });
+
+    navPjt.addEventListener("click", () => {
+        dropdown.classList.toggle('dropdown-active');
+    });
+
+    pjtList.forEach((a) => {
+        a.addEventListener('click', () => {
+            dropdown.classList.remove('dropdown-active');
+        })
+    })
 }
 
 init();
